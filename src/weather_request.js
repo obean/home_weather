8c80d04711d8aa9b2505587f900869280d0996cb
ollie beney <ombeney@gmail.com>
2020-10-14 23:01:16 +0100

    did some research around the rain measurements in hello.hourly. added it in messy comments at the botttom, signing off

--- src/weather_request.js
+++ src/weather_request.js
@@ -53,7 +53,7 @@ $('#locale').text(weather.list[0].main.temp)})//.main.temp-273.15))})
 $.get("http://api.openweathermap.org/data/2.5/onecall?lat=51.3002&lon=0.4933&appid=" + OPEN_WEATHER_API, function (weather){
   console.log(weather)
 $('#forecast').text(weather.current.weather[0].description)  
- hello = weather.minutely
+ hello = weather
 $('#precip').text(addWeatherData.parsePrecip(weather.minutely)[0])
 if((addWeatherData.parsePrecip(weather.minutely)[0])){
 
@@ -64,3 +64,10 @@ $('#timeRain').text(addWeatherData.parsePrecip(weather.minutely)[1])}
 // $.get("http://api.openweathermap.org/data/2.5/weather?q=London&appid=16f42bf1b7c2d1ff6c5fba304885dc11", function (weather)
 //     {$('#locale').text(Math.round(weather.main.temp-273.15))})
 //https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
+
+// function in browser to get hours where rain expected below
+// for(let i =0; i <= hello.hourly.length-1; i++){
+//   if( hello.hourly[i].rain != undefined){
+//       console.log(i)}}
+// this link says that rain measurements are probably milimiters
+//https://www.google.com/search?q=metric+measurement+for+rain&rlz=1C5CHFA_enGB844GB844&oq=metric+measurement+for+rain&aqs=chrome..69i57j0i22i30i457.8263j0j9&sourceid=chrome&ie=UTF-8
